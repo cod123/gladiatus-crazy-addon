85310dea92c447f79d9588035097a4aba7be0958
GreatApo@gmail.com <GreatApo@gmail.com@4983dbdc-05d7-5317-ccbf-71ad76f46445>
2011-06-04 10:25:28 +0000

    Split the main.js, file:13

---
+++ trunk/[13]itemsYouCanBuy.js
@@ -0,0 +1,42 @@
+﻿//################################################################################################################################
+//## ITEMS THAT YOU CAN BUY / ΑΝΤΙΚΕΙΜΕΝΑ ΠΟΥ ΜΠΟΡΕΙΣ ΝΑ ΑΓΟΡΑΣΕΙΣ
+//################################################################################################################################
+function afford(){
+	if(document.getElementById('shop')){
+		var item=document.getElementById('shop').getElementsByTagName('div')[1].id.match(/.\d+/i);
+		
+		var gold=document.getElementById('sstat_gold_val').innerHTML.replace(/\./g,'').match(/\d+/i)*1;
+		var rubi=document.getElementById('sstat_ruby_val').innerHTML.replace(/\./g,'').match(/\d+/i)*1;
+		for (i = 0; i <=6; i++){
+			for (j = 0; j <=8; j++){
+				if(document.getElementById('tOoLtIp_'+item+'_'+i+'_'+j)){
+					if(document.getElementById('tOoLtIp_'+item+'_'+i+'_'+j).innerHTML.replace(/\./gi,'').match(/(\d+) <img[^\/]+\/img\/res3/i)){
+						document.getElementById(item+'_'+i+'_'+j).style.background='rgba(255, 0, 0, 0.2)';
+						if(document.getElementById('tOoLtIp_'+item+'_'+i+'_'+j).innerHTML.replace(/\./gi,'').match(/(\d+) <img[^\/]+\/img\/res3/i)[1]*1>rubi*1){
+							document.getElementById(item+'_'+i+'_'+j).style.opacity='0.25';
+						}
+					}
+					else{
+						document.getElementById(item+'_'+i+'_'+j).style.opacity='1.00';
+					}
+					
+					if(document.getElementById('tOoLtIp_'+item+'_'+i+'_'+j).innerHTML.replace(/\./gi,'').match(/(\d+) <img[^\/]+\/img\/res2/i)){
+						if(document.getElementById('tOoLtIp_'+item+'_'+i+'_'+j).innerHTML.replace(/\./gi,'').match(/(\d+) <img[^\/]+\/img\/res2/i)[1]*1>gold*1){
+							document.getElementById(item+'_'+i+'_'+j).style.opacity='0.25';
+						}
+					}
+					else{
+						document.getElementById(item+'_'+i+'_'+j).style.opacity='1.00';
+					}
+				}
+			}
+		}
+	}
+}
+function reAfford(){
+	var oldSetToolTip = unsafeWindow.SetToolTip;
+	unsafeWindow.SetToolTip = function (updateTooltip){
+		if(updateTooltip){setTimeout(afford, 900);}
+		return oldSetToolTip(updateTooltip);
+	}
+}
