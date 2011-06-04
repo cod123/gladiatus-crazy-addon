1af1841498d416574c292f887a2df398496f1a22
Apo59m@gmail.com <Apo59m@gmail.com@4983dbdc-05d7-5317-ccbf-71ad76f46445>
2011-06-04 10:12:26 +0000

    Split the main/js, file:1

---
+++ trunk/[1]generalValues.js
@@ -0,0 +1,27 @@
+﻿// ==UserScript==
+// @name           Gladiatus Crazy AddOn New Server
+// @namespace      http://www.epiratiko.webs.com/ 
+// @description    The craziest add-on for gladiatus ever!
+// @include        *.gladiatus.fr*
+// @exclude        *board.gladiatus.*
+// ==/UserScript==
+//Copyright (C) 2010 - 2011, Apo59m, DarkThanos and djor
+
+//################################################################################################################################
+//## GENERAL SCRIPT'S VALUES / ΓΕΝΙΚΕΣ ΜΕΤΑΒΛΗΤΕΣ ΤΟΥ SCRIPT
+//################################################################################################################################
+var version = "240";
+if(document.location.href.match(/^(.*\?)mod=(\w+).*sh=([0-9a-fA-F]+)/)){
+	var GCAO_result = document.location.href.match(/^(.*\?)mod=(\w+).*sh=([0-9a-fA-F]+)/);
+	var GCAO_allresult = GCAO_result[0]; //ex.: http://s3.gladiatus.gr/game/index.php?mod=overview&sh=3311f55ccc62c53c42170cdf78b1cf59
+	var GCAO_siteurl = GCAO_result[1]; //ex.: http://s3.gladiatus.gr/game/index.php?
+	var GCAO_mod = GCAO_result[2];
+	var GCAO_secureCode = GCAO_result[3];
+	var UrlOverview = GCAO_siteurl + 'mod=overview&sh=' + GCAO_secureCode;
+	var UrlUpdate = "http://epiratiko.webs.com/GCAO/UpdateGCAO.htm";
+	if(document.location.href.match(/http\:\/\/s(\d+)/i)){var GCAO_server = document.location.href.match(/http\:\/\/s(\d+)/i)[1];}
+	else{var GCAO_server = "1";}
+}
+if(document.location.href.match(/submod=(\w+)/)){var GCAO_submod = document.location.href.match(/submod=(\w+)/)[1];}
+else{var GCAO_submod = 'noSubmod';}
+var day=new Date();
