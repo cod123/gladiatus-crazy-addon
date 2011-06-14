259d9133d3fcc9643e70303e74339b1f557b459f
Apo59m@gmail.com <Apo59m@gmail.com@4983dbdc-05d7-5317-ccbf-71ad76f46445>
2011-06-14 16:42:22 +0000

    Small changes to match with the [3]addonManager.js

--- trunk/djor's new features.js
+++ trunk/djor's new features.js
@@ -1,18 +1,18 @@
-
-
-
-// Improving guild war icons (using icons of quests)
-if((GCAO_mod=='guild_warcamp')&&(GCAO_submod=='guild_combatreports')){
+//################################################################################################################################
+//## IMPROVING GUILD WAR ICONS (USING ICONS OF QUESTS)
+//################################################################################################################################
+function betterGuildWarIcons(){
 	document.getElementById('content').innerHTML = document.getElementById('content').innerHTML.replace(/guild\/combat\/lose\.gif/gi,'ui/quest/button_cancel.jpg" width="20"');
 	document.getElementById('content').innerHTML = document.getElementById('content').innerHTML.replace(/guild\/combat\/win\.gif/gi,'ui/quest/button_finish.jpg" width="20"');
 	document.getElementById('content').innerHTML = document.getElementById('content').innerHTML.replace(/guild\/combat\/draw\.gif/gi,'ui/quest/button_accept.jpg" width="20"');
 }
 
-
-// Problem : it displays a [!] which shows you new messages (not totally ok), however it launches a xhr and visits the message page, so at the next visited page, there 
-// will be no new messages, even if you didn't go at the message page... There is the same problem with expired packages, it visits package's page, so the count of new 
+//################################################################################################################################
+//## SEE NEW MESSAGE
+//################################################################################################################################
+// Problem : it displays a [!] which shows you new messages (not totally ok), however it launches a xhr and visits the message page, so at the next visited page, there
+// will be no new messages, even if you didn't go at the message page... There is the same problem with expired packages, it visits package's page, so the count of new
 // package returns to 0... 
-seeNewMessage();
 function seeNewMessage(){
 	var divs = document.evaluate(".//a[@id='menue_messages']/div[@class='menue_new_count']",document.body,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null);
 	if(divs.snapshotLength==1){
@@ -30,7 +30,6 @@ function seeNewMessage(){
 				warningTip.setAttribute('style', 'opacity: 0.75;display:none;z-index:502;position:absolute;top:179px;left:345px;border:1px solid grey;background-color:black;color:#c0c0c0;font-weight:bold;line-height:24px;padding-left:5px;padding-right:5px;')
 				document.getElementById('header_game').appendChild(warningTip);
 					
-				
 				var messagesToShow = "";
 				var i =1;
 				while((numberOfNewMessages>1)&&(i<26)){
@@ -45,10 +44,7 @@ function seeNewMessage(){
 				}
 				
 				warningTip.innerHTML += '<table>'+messagesToShow.replace(/<tr/g,'<tr style="text-align:left"')+'</table>';
-				
 			}
 		});
-		
-		
 	}
 }
