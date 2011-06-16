3e5ea6563636f061170b79b2b0eb95ba43c71a65
GreatApo@gmail.com <GreatApo@gmail.com@4983dbdc-05d7-5317-ccbf-71ad76f46445>
2011-06-16 13:38:24 +0000

    BUG FIX: Problem -> Missing Secure Code from Url Solution -> Take Secure Code from "href" attribute of message button Report -> Error: Component returned failure code: 0x804b000a (NS_ERROR_MALFORMED_URI) [nsIIOService.extractScheme] 	  Error Link :http://s7.gladiatus.no/game/index.php?mod=powerups

--- trunk/[1]generalValues.js
+++ trunk/[1]generalValues.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+ // ==UserScript==
 // @name           Gladiatus Crazy AddOn New Server
 // @namespace      http://www.epiratiko.webs.com/ 
 // @description    The craziest add-on for gladiatus ever!
@@ -10,13 +10,25 @@
 //################################################################################################################################
 //## GENERAL SCRIPT'S VALUES / ΓΕΝΙΚΕΣ ΜΕΤΑΒΛΗΤΕΣ ΤΟΥ SCRIPT
 //################################################################################################################################
-var version = "240";
-if(document.location.href.match(/^(.*\?)mod=(\w+).*sh=([0-9a-fA-F]+)/)){
-	var GCAO_result = document.location.href.match(/^(.*\?)mod=(\w+).*sh=([0-9a-fA-F]+)/);
+var doc=document;
+var version = "250";
+if(document.location.href.match(/^(.*\?)mod=(\w+)/)){
+	if(document.location.href.match(/^(.*\?)mod=(\w+).*sh=([0-9a-fA-F]+)/)){
+		var GCAO_result = document.location.href.match(/^(.*\?)mod=(\w+).*sh=([0-9a-fA-F]+)/);
+		var GCAO_mod = GCAO_result[2];
+		var GCAO_secureCode = GCAO_result[3];
+	}else{
+		var GCAO_result = document.location.href.match(/^(.*\?)mod=(\w+)/);
+		var GCAO_mod = GCAO_result[2];
+		if(document.getElementById('menue_messages')){
+			var GCAO_secureCode = document.getElementById('menue_messages').getAttribute('href').match(/sh=([0-9a-fA-F]+)/)[1];
+		}else{
+			var GCAO_secureCode = 'error';
+		}
+	}
 	var GCAO_allresult = GCAO_result[0]; //ex.: http://s3.gladiatus.gr/game/index.php?mod=overview&sh=3311f55ccc62c53c42170cdf78b1cf59
 	var GCAO_siteurl = GCAO_result[1]; //ex.: http://s3.gladiatus.gr/game/index.php?
-	var GCAO_mod = GCAO_result[2];
-	var GCAO_secureCode = GCAO_result[3];
+	
 	var UrlOverview = GCAO_siteurl + 'mod=overview&sh=' + GCAO_secureCode;
 	var UrlUpdate = "http://epiratiko.webs.com/GCAO/UpdateGCAO.htm";
 	if(document.location.href.match(/http\:\/\/s(\d+)/i)){var GCAO_server = document.location.href.match(/http\:\/\/s(\d+)/i)[1];}
