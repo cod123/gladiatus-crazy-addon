e497ab04b7211511d8a3858d2c652b5b414662ad
GreatApo@gmail.com <GreatApo@gmail.com@4983dbdc-05d7-5317-ccbf-71ad76f46445>
2011-06-16 13:20:45 +0000

    New Message Alert Changes: - I removed the checkboxes - [!] Icon moved over message icon (left down corner) - Onmouseover, the box opens and if it's already open, it closes

--- trunk/djor's new features.js
+++ trunk/djor's new features.js
@@ -17,17 +17,18 @@ function seeNewMessage(){
 	var divs = document.evaluate(".//a[@id='menue_messages']/div[@class='menue_new_count']",document.body,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null);
 	if(divs.snapshotLength==1){
 		var numberOfNewMessages = divs.snapshotItem(0).innerHTML.replace(/[^0-9]/gi,'');
-		// var numberOfNewMessages=2;
+		//var numberOfNewMessages=2;
+		var messageUrl=document.getElementById('menue_messages').getAttribute('href').replace('index.php?','');
 		GM_xmlhttpRequest({
 			method: "GET",
-			url: GCAO_siteurl + 'mod=messages&submod=messageShow&folder=0&sh='+GCAO_secureCode,
+			url: GCAO_siteurl + messageUrl,
 			onload: function(response){
-				
+				response.responseText=response.responseText.replace(/type="checkbox"/gi,'type="hidden"').replace(/width="30px"/gi,'width="0px"');
 				var messages = response.responseText.split(/<tr class="alt">/g);
-				document.getElementById('mainmenu').getElementsByTagName('a')[0].innerHTML+=' <img src="http://i617.photobucket.com/albums/tt260/goldisever/GCAO/exclamation.png" style="position:absolute;margin-left:10px;margin-top:1px;opacity: 0.65;" onmouseover="this.style.opacity=\'1\';document.getElementById(\'warningWeapon\').style.display=\'block\';">';
+				document.getElementById('menue_messages').innerHTML+=' <img src="http://i617.photobucket.com/albums/tt260/goldisever/GCAO/exclamation.png" style="position:absolute;margin-left:-20px;margin-top:19px;opacity: 0.65;" onmouseover="if(document.getElementById(\'warningMessage\').style.display!=\'block\'){this.style.opacity=\'1\';document.getElementById(\'warningMessage\').style.display=\'block\';}else{this.style.opacity=\'0.65\';document.getElementById(\'warningMessage\').style.display=\'none\';}">';
 				var warningTip = document.createElement('div');
-				warningTip.setAttribute('id', 'warningWeapon');
-				warningTip.setAttribute('style', 'opacity: 0.75;display:none;z-index:502;position:absolute;top:179px;left:345px;border:1px solid grey;background-color:black;color:#c0c0c0;font-weight:bold;line-height:24px;padding-left:5px;padding-right:5px;')
+				warningTip.setAttribute('id', 'warningMessage');
+				warningTip.setAttribute('style', 'opacity: 0.75;display:none;z-index:502;position:absolute;top:85px;left:255px;border:1px solid grey;background-color:black;color:#c0c0c0;font-weight:bold;line-height:24px;padding-left:5px;padding-right:5px;')
 				document.getElementById('header_game').appendChild(warningTip);
 					
 				var messagesToShow = "";
